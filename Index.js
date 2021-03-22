e538efacdbca2ee97288c4640313f0f774bc1d9b
Alexis Rojas <heyderrojas2003@gmail.com>
2021-03-22 13:38:08 -0500

    Gariuz firs deploy

---
+++ Index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.set("view engine","ejs");
+app.set("views",__dirname + "/views");
+
+//Formulario.
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+app.use(require("./routes/index"));
+
+//General.
+app.use(express.static(__dirname+"/public"));
+
+app.get("/",(req,res)=>{
+    res.render("Index",{titulo:""});
+});
+
+app.get("/Servicios",(req,res)=>{
+    res.render("Servicios");
+});
+
+/*Nosotros*/
+app.get("/Somos",(req,res)=>{
+    res.render("Somos");
+});
+app.get("/Filosofia",(req,res)=>{
+    res.render("Filosofia");
+});
+app.get("/Historia",(req,res)=>{
+    res.render("Historia");
+});
+app.get("/Calidad-y-garantia",(req,res)=>{
+    res.render("Calidad");
+});
+
+
+app.listen(port,()=>{
+    console.log("Server",port,"running...");
+})
---
+++ routes/Index.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const {google} = require("googleapis");
+const router = express.Router();
+
+router.post("/send-email", (req,res) => {
+    const {name, email, phone, service, message, upload} = req.body;
+    const contentHTML = `
+    <div style= "background-color:  #112934; color: #fdf6e6; margin: 20px; padding:40px">
+    <h1 style= "text-align: center; border-bottom: 2px solid #fdf6e6">Formulario.</h1>
+    <ul style= "list-style: none;">
+    <li style= "padding-top: 10px;"><b>Nombre:</b> ${name}</li>
+    <li style= "padding-top: 10px;"><b>Correo:</b> ${email}</li>
+    <li style= "padding-top: 10px;"><b>Numero celular:</b> ${phone}</li>
+    <li style= "padding-top: 10px;"><b>Servicio:</b> ${service}</li>
+    <li style= "padding-top: 10px;"><b>Descripción:</b><p style="margin-right:30px; border-bottom:2px solid #b70e21; margin-left:10px">${message}</p></li>
+    </ul>
+    `
+
+    const CLIENT_ID = "908110287431-dvenc4k896lspgj85qjd159tlaij9f0s.apps.googleusercontent.com";
+    const CLIENT_SECRET = "JNL6w2oboW8ZxPT8N2MPiB0f";
+    const REDIRECT_URI = "https://developers.google.com/oauthplayground";
+    const REFRESH_TOKEN = 
+        "1//04OVgYmNB0QTsCgYIARAAGAQSNwF-L9IrnbjFU7oUAnTdFIHbPaPzrQga4b9mMQ9odgNZSlP-UpV0u_m4RYWLxTXwIgI8WW1b3LI"; 
+
+    const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+
+    oAuth2Client.setCredentials({refresh_token: REFRESH_TOKEN });
+
+    async function sendMail(){
+        
+        try{
+            const accessToken = await oAuth2Client.getAccessToken();
+            const transporter = nodemailer.createTransport({
+                service: "gmail",
+                auth:{
+                    type: "OAuth2",
+                    user: "gariuzemails@gmail.com",
+                    clientId: CLIENT_ID,
+                    clientSecret: CLIENT_SECRET,
+                    refreshToken: REFRESH_TOKEN,
+                    accessToken: accessToken,
+                },
+            });
+            const mailOptions = {
+                from: "Contacto Gariuz emails.",
+                to: "gariuzconstructor11@gmail.com",
+                subject: `${service}`,
+                html: contentHTML,
+            };
+
+            const result = await transporter.sendMail(mailOptions);
+            return result;
+
+        }catch(err){
+            console.log(err);
+        }
+    }
+    sendMail()
+        .then((result) => res.redirect("/Sucess.html"))
+        .catch((error) => console.log(error.message));
+});
+
+module.exports = router;
