9e35eb121a9aba9fe649caf7c1084f16c3f2603c
Alexis Rojas <heyderrojas2003@gmail.com>
2021-03-23 07:29:17 -0500

    Cambio 12

--- Index.js
+++ Index.js
@@ -9,34 +9,31 @@ app.set("views",__dirname + "/views");
 //Formulario.
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
-app.use(require("./routes/Index.js"));
+app.use(require("./routes/index.js"));
 
 //General.
-app.use(express.static(__dirname+"./public"));
+app.use(express.static(__dirname+"/public"));
 
 app.get("/",(req,res)=>{
     res.render("Index");
 });
 
-app.get("/Servicios",(req,res)=>{
+app.get("/servicios",(req,res)=>{
     res.render("Servicios");
 });
 
 /*Nosotros*/
-app.get("/Somos",(req,res)=>{
+app.get("/somos",(req,res)=>{
     res.render("Somos");
 });
-app.get("/Filosofia",(req,res)=>{
-    res.render("Filosofia");
-});
-app.get("/Historia",(req,res)=>{
+app.get("/historia",(req,res)=>{
     res.render("Historia");
 });
-app.get("/Calidad-y-garantia",(req,res)=>{
+app.get("/calidad-y-garantia",(req,res)=>{
     res.render("Calidad");
 });
 
 
-app.listen(port,()=>{
+app.listen(port,() => {
     console.log("Server",port,"running...");
 })
